543d4ad5aaa9f6829afee0005a07416f5bb06dde
Joseph Dykstra <josephdykstra@gmail.com>
2015-06-09 21:32:45 -0500

    cleaned up debug test

--- test/test.js
+++ test/test.js
@@ -2,15 +2,4 @@ var parseSong = require('../js/parse-song/index.js')
 var fs = require('fs')
 
 var songFile = fs.readFileSync(__dirname + '/amazing-grace.md', { encoding: 'utf8' })
-
-// parseSong(songFile).forEach(displaySlide)
 parseSong(songFile).forEach(console.log)
-
-function displaySlide(slide, i) {
-	if (i) console.log('-----')
-	console.log(
-		slide.header +
-		'\n\t' + slide.lyrics.slice(0, 15) + '...\n' +
-		slide.footer
-	)
-}
