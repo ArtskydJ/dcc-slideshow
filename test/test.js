1ee173aa5864300ddcf5885c6dd989c7ad0a2d46
Joseph Dykstra <josephdykstra@gmail.com>
2015-06-09 23:56:56 -0500

    moved files from /test to /public/test-content

--- test/test.js
+++
@@ -1,5 +0,0 @@
-var parseSong = require('../js/parse-song/index.js')
-var fs = require('fs')
-
-var songFile = fs.readFileSync(__dirname + '/amazing-grace.md', { encoding: 'utf8' })
-parseSong(songFile).forEach(console.log)
