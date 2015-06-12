d1e0883d63ec028306e4b1681dd7c1015305d9a0
Joseph Dykstra <josephdykstra@gmail.com>
2015-06-11 20:19:54 -0500

    using new project-parser api

--- js/view/index.js
+++ js/view/index.js
@@ -1,11 +1,10 @@
 var hashChange = require('hash-change')
-var projectParser = require('../parse-project/index.js')
+var ProjectParser = require('../parse-project/index.js')
 
-var PROJECT_URL = 'http://localhost/test-content/project.txt'
-var SONGS_URL = 'http://localhost/test-content/'
+var pp = ProjectParser('http://localhost/test-projects/', 'http://localhost/test-songs/')
 
 module.exports = function domSlide(element) {
-	projectParser(PROJECT_URL, SONGS_URL, function (err, htmlSlides) {
+	pp('project.txt', function (err, htmlSlides) {
 		if (err) {
 			console.error(err)
 		} else {
