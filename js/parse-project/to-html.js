7262ca3f8b27803dbb2cddeb55b9b13fdba1c280
Joseph Dykstra <josephdykstra@gmail.com>
2015-06-11 20:20:11 -0500

    removed extraneous comma

--- js/parse-project/to-html.js
+++ js/parse-project/to-html.js
@@ -4,22 +4,18 @@ module.exports = function htmlify(slide) {
 		'<div class="lyrics">',
 		'\t' + slide.lyrics.replace(/\r?\n/g, '<br>\t'),
 		'</div>',
-		'<div class="footer">' + slide.footer + '</div>',
+		'<div class="footer">' + slide.footer + '</div>'
 	].join('\n')
 }
 
 
 /*
-<div class="header">
-	Amazing Grace
-</div>
+<div class="header">Amazing Grace</div>
 <div class="lyrics">
 	Amazing Grace, how sweet the sound,<br>
 	that saved a wretch like me.<br>
 	I once was lost, but now I&#39;m found<br>
 	was blind, but now I see.
 </div>
-<div class="footer">
-	Public Domain (maybe)
-</div>
+<div class="footer">Public Domain (maybe)</div>
 */
