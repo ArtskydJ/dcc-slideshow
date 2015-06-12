80aee1992e6831c5d4b5bd8cccb793030b0e1709
Joseph Dykstra <josephdykstra@gmail.com>
2015-06-12 14:27:52 -0500

    renamed image-stream.js -> data-uri-stream.js

--- js/view/get.js
+++ js/view/get.js
@@ -1,8 +1,9 @@
 var hyperquest = require('hyperquest')
-var concat = require('concat-stream')
+var concatStream = require('concat-stream')
 var url = require('url')
 var onetime = require('onetime')
-var ImageStream = require('./image-stream.js')
+var DataUriStream = require('./data-uri-stream.js')
+var isImage = require('is-image')
 
 module.exports = function get(urlBase, urlFile, callback) {
 	var cb = onetime(callback)
@@ -11,10 +12,14 @@ module.exports = function get(urlBase, urlFile, callback) {
 		if (err) {
 			cb(err)
 		} else {
-			res.pipe(ImageStream(urlFile))
-				.pipe(concat({ encoding: 'string' }, function (data) {
-					cb(null, data)
-				}))
+			var concat = concatStream({ encoding: 'string' }, function (data) {
+				cb(null, data)
+			})
+			if (isImage(urlFile)) {
+				res.pipe(DataUriStream()).pipe(concat)
+			} else {
+				res.pipe(concat)
+			}
 			res.on('error', cb)
 		}
 	})
