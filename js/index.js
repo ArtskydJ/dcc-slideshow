d9f2346421c2d7afd0cd7a9a4246707d2e290c4c
Joseph Dykstra <josephdykstra@gmail.com>
2015-12-31 09:24:28 -0600

    Merged presentation and preview into one file

--- js/index.js
+++ js/index.js
@@ -4,18 +4,21 @@ var elementClass = require('element-class')
 var emitter = require('tab-emitter')('slides')
 
 var rootElement = document.body
-var reelElement = document.getElementById('reel')
-var mainElement = document.getElementById('main')
+var mainElement = document.body
+if (window.PREVIEW_MODE) {
+	var reelElement = document.getElementById('reel')
+	mainElement = document.getElementById('main')
+}
 var getSlideNodes = SlideNodes('http://localhost/test-projects/', 'http://localhost/test-songs/')
 
 getSlideNodes('project.txt').then(function (nodes) {
 	var max = nodes.length - 1
 	nodes.forEach(function (node, i) {
-		var clone = node.cloneNode(true)
-
-		//elementClass(clone).add('hide')
-		clone.id = 1000 + i
-		reelElement.appendChild(clone)
+		if (window.PREVIEW_MODE) {
+			var clone = node.cloneNode(true)
+			clone.id = 1000 + i
+			reelElement.appendChild(clone)
+		}
 
 		elementClass(node).add('hide')
 		node.id = i
@@ -23,10 +26,11 @@ getSlideNodes('project.txt').then(function (nodes) {
 	})
 
 	controls(rootElement, emitter, max, function (slideId) {
-		elementClass(reelElement.querySelector('.show')).remove('show')
-		elementClass(mainElement.querySelector('.show')).remove('show')
+		elementClass(rootElement.querySelector('.show')).remove('show')
 
-		elementClass(document.getElementById(1000 + slideId)).add('show')
+		if (window.PREVIEW_MODE) {
+			elementClass(document.getElementById(1000 + slideId)).add('show')
+		}
 		elementClass(document.getElementById(slideId)).add('show')
 	})
 }).catch(function (err) {
