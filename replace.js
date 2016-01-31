f6ebeb072df3e98056523cd3825e9df96f05d968
Joseph Dykstra <josephdykstra@gmail.com>
2016-01-30 23:02:54 -0600

    Make it more obvious that non-spaces are getting replaced

--- replace.js
+++ replace.js
@@ -4,8 +4,11 @@ var path = require('path')
 fs.readdirSync(path.join(__dirname, 'txt')).forEach(function (filename) {
 	var realPath = path.join(__dirname, 'txt', filename)
 	var contents = fs.readFileSync(realPath, 'utf8')
-	if (contents.indexOf(' ') !== -1) {
-		fs.writeFileSync(realPath, contents.replace(/ /g, ' '))
+
+	// Had some issues with non-breaking spaces. (Char code 160, hex A0.)
+	// This finds occurrences of non-breaking spaces and replaces them with normal spaces.
+	if (contents.indexOf(String.fromCharCode(160)) !== -1) {
+		fs.writeFileSync(realPath, contents.replace(/\xA0/g, ' '))
 		console.log('Fixed ' + filename)
 	}
 })
