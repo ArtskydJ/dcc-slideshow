f8e279dbc97297f560d2ae067788ac95b7d0edc5
Joseph Dykstra <josephdykstra@gmail.com>
2016-11-10 22:40:14 -0600

    ES6-ify replace and scan

--- replace.js
+++ replace.js
@@ -1,14 +1,16 @@
+// Had some issues with non-breaking spaces. (Char code 160, hex A0.)
+// This finds occurrences of non-breaking spaces and replaces them with normal spaces.
+
 var fs = require('fs')
 var path = require('path')
 
-fs.readdirSync(path.join(__dirname, 'txt')).forEach(function (filename) {
-	var realPath = path.join(__dirname, 'txt', filename)
-	var contents = fs.readFileSync(realPath, 'utf8')
+var srcPath = path.join(__dirname, 'txt')
+var destPath = path.join(__dirname, 'txt')
 
-	// Had some issues with non-breaking spaces. (Char code 160, hex A0.)
-	// This finds occurrences of non-breaking spaces and replaces them with normal spaces.
-	if (contents.indexOf(String.fromCharCode(160)) !== -1) {
-		fs.writeFileSync(realPath, contents.replace(/\xA0/g, ' '))
+fs.readdirSync(srcPath)
+	.map(filename => ({ filename, contents: fs.readFileSync(path.join(srcPath, filename), 'utf-8') }))
+	.filter(({ contents }) => contents.includes(String.fromCharCode(160)))
+	.forEach(({ contents, filename }) => {
+		writeFile(filename, fs.writeFileSync(path.join(destPath, filename), contents.replace(/\xA0/g, ' ')))
 		console.log('Fixed ' + filename)
-	}
-})
+	})
--- scan.js
+++ scan.js
@@ -2,12 +2,10 @@ var fs = require('fs')
 var path = require('path')
 var openr = require('opener')
 
+var srcPath = path.join(__dirname, 'txt')
+
 var REGEX = /[^©A-Za-z0-9\r\n\.!,;: '"\/#\(\)>?&—…\[\]-]/g
 
-fs.readdirSync(path.join(__dirname, 'txt')).filter(function (filename) {
-	var contents = fs.readFileSync(path.join(__dirname, 'txt', filename), 'utf8')
-	return REGEX.test(contents)
-}).forEach(function (filename) {
-	// console.log('Has special chars ' + filename + ' -> ' + contents.match(REGEX).join())
-	openr(path.join(__dirname, 'txt', filename))
-})
+fs.readdirSync(srcPath)
+	.filter(filename => REGEX.test( fs.readFileSync(path.join(srcPath, filename), 'utf8') ))
+	.forEach(filename => openr(path.join(srcPath, filename)) )
